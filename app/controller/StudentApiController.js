43c88fa2efce3e3653c060dfe06d18cc51dbe666
RUSSELLS-VIPER <arijithati@proton.me>
2025-04-16 16:14:16 +0530

    crud application done

--- app/controller/StudentApiController.js
+++ app/controller/StudentApiController.js
@@ -44,6 +44,74 @@ class StudentApiController{
     }
 
 
+    async editStudent(req, res) {
+        try {
+            const id = req.params.id
+            const editdata = await Student.findById(id)
+            if (editdata) {
+                res.status(200).json({
+                    message:"get single data",
+                    data: editdata
+                })
+                
+            } else {
+                res.status(404).json({
+                    message: "id not found",
+                  
+                })
+                
+            }
+           
+            
+        } catch (error) {
+            res.status(500).json({
+                message: error.message
+            })
+        }
+    }
+
+    async updateStudent(req, res) {
+        try {
+            const id = req.params.id
+            const {name,email,phone}=req.body
+            const updatedata = await Student.findByIdAndUpdate(id, {
+                name,email,phone
+            })
+          
+                res.status(200).json({
+                    message:"data update successfully",
+                   
+                })
+           
+            
+        } catch (error) {
+            res.status(500).json({
+                message: error.message
+            })
+        }
+    }
+
+
+    async deleteStudent(req, res) {
+        try {
+            const id = req.params.id
+           
+            const updatedata = await Student.findByIdAndDelete(id)
+          
+                res.status(200).json({
+                    message:"data delete successfully"
+                   
+                })
+           
+            
+        } catch (error) {
+            res.status(500).json({
+                message: error.message
+            })
+        }
+    }
+
+
 }
 
 module.exports= new StudentApiController()
--- app/router/StudentApiRoute.js
+++ app/router/StudentApiRoute.js
@@ -7,6 +7,10 @@ const router = express.Router()
 
 router.post('/create/student',StudentApiController.craeteStudent)
 router.get('/student',StudentApiController.getStudent)
+router.get('/student/edit/:id',StudentApiController.editStudent)
+router.get('/student/edit/:id',StudentApiController.editStudent)
+router.post('/student/update/:id',StudentApiController.updateStudent)
+router.delete('/student/delete/:id',StudentApiController.deleteStudent)
 
 
 
