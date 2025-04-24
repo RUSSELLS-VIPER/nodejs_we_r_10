ecbb5338eff1da8f2b94771637a9b3b2b1fdba6e
Pritam Adhikari <pritam762.hitcsecs2020@gmail.com>
2025-04-24 14:47:47 +0530

    joi

--- app/controller/StudentApiController.js
+++ app/controller/StudentApiController.js
@@ -1,123 +1,106 @@
-const Student = require('../model/student')
-const fs=require('fs')
+const { StudentModel, StudentSchemaValidate } = require("../model/student");
+const fs = require("fs");
 
-class StudentApiController{
+class StudentApiController {
+  async craeteStudent(req, res) {
+    //console.log(req.file);
 
-
-    async craeteStudent(req, res) {
-        //console.log(req.file);
-        
-        try {
-            const { name, email, phone } = req.body
-            
-            const studentdata=new Student({
-               name,email,phone
-
-            })
-            if (req.file) {
-                studentdata.image = req.file.path
-            }
-
-            const data = await studentdata.save()
-            res.status(200).json({
-                message: "data added successfully",
-                data:data
-            })
-            
-        } catch (error) {
-            res.status(500).json({
-                message:error.message
-            })
-        }
+    try {
+      const data = {
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.phone,
+      };
+      //validating the request
+      const { error, value } = StudentSchemaValidate.validate(data);
+      if (error) {
+        return res.status(401).json({
+          message: error.details[0].message,
+        });
+      } else {
+        const stu = new StudentModel(value)
+        const dd=await stu.save()
+        return res.status(201).json({
+          message: "student created successfully",
+          data: dd,
+        });
+      }
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-
-    async getStudent(req, res) {
-        try {
-            
-            const data=await Student.find()
-            res.status(200).json({
-                message: "data fetch successfully",
-                total:data.length,
-                data:data
-            })
-            
-        } catch (error) {
-            res.status(500).json({
-                message:error.message
-            })
-        }
+  async getStudent(req, res) {
+    try {
+      const data = await StudentModel.find();
+      res.status(200).json({
+        message: "data fetch successfully",
+        total: data.length,
+        data: data,
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: error.message,
+      });
     }
+  }
 
-
-    async editStudent(req, res) {
-        try {
-            const id = req.params.id
-            const editdata = await Student.findById(id)
-            if (editdata) {
-                res.status(200).json({
-                    message:"get single data",
-                    data: editdata
-                })
-                
-            } else {
-                res.status(404).json({
-                    message: "id not found",
-                  
-                })
-                
-            }
-           
-            
-        } catch (error) {
-            res.status(500).json({
-                message: error.message
-            })
-        }
+  async editStudent(req, res) {
+    try {
+      const id = req.params.id;
+      const editdata = await StudentModel.findById(id);
+      if (editdata) {
+        res.status(200).json({
+          message: "get single data",
+          data: editdata,
+        });
+      } else {
+        res.status(404).json({
+          message: "id not found",
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        message: error.message,
+      });
     }
+  }
 
-    async updateStudent(req, res) {
-        try {
-            const id = req.params.id
-            const {name,email,phone}=req.body
-            const updatedata = await Student.findByIdAndUpdate(id, {
-                name,email,phone 
-            })
-          
-                res.status(200).json({
-                    message:"data update successfully",
-                   
-                })
-           
-            
-        } catch (error) {
-            res.status(500).json({
-                message: error.message
-            })
-        }
+  async updateStudent(req, res) {
+    try {
+      const id = req.params.id;
+      const { name, email, phone } = req.body;
+      const updatedata = await StudentModel.findByIdAndUpdate(id, {
+        name,
+        email,
+        phone,
+      });
+
+      res.status(200).json({
+        message: "data update successfully",
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: error.message,
+      });
     }
+  }
 
+  async deleteStudent(req, res) {
+    try {
+      const id = req.params.id;
 
-    async deleteStudent(req, res) {
-        try {
-            const id = req.params.id
-           
-            const updatedata = await Student.findByIdAndDelete(id)
-          
-                res.status(200).json({
-                    message:"data delete successfully"
-                   
-                })
-           
-            
-        } catch (error) {
-            res.status(500).json({
-                message: error.message
-            })
-        }
+      const updatedata = await StudentModel.findByIdAndDelete(id);
+
+      res.status(200).json({
+        message: "data delete successfully",
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: error.message,
+      });
     }
-
-
+  }
 }
 
-module.exports= new StudentApiController()
+module.exports = new StudentApiController();
--- app/model/student.js
+++ app/model/student.js
@@ -1,29 +1,52 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const Joi = require("joi");
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
+//validation schema
+const StudentSchemaValidate = Joi.object({
+  name: Joi.string().min(3).max(30).required(),
 
-const StudentSchema=new Schema({
+  email: Joi.string()
+    .required()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net", "in"] } }),
+
+  phone:
+    Joi.string()
+    .regex(/^[0-9]{10}$/),
+  // .custom((value, helper) => {
+  //     // you can use any libs for check phone
+  //     if (!checkPhone(value)) {
+  //         return helper.message("phone is incorrect")
+
+  //     return value
+  //     }
+  // })
+});
+
+const StudentSchema = new Schema(
+  {
     name: {
-        type: String,
-        require:true
+      type: String,
+      require: true,
     },
     email: {
-        type: String,
-        require:true
+      type: String,
+      require: true,
     },
     phone: {
-        type: String,
-        require:true
+      type: String,
+      require: true,
     },
     image: {
-        type: String,
-        require:true
+      type: String,
+      default: "image",
     },
-}, {
-    timestamps:true
-})
+  },
+  {
+    timestamps: true,
+  }
+);
 
-
-const StudentModel = mongoose.model('student', StudentSchema)
-module.exports=StudentModel
+const StudentModel = mongoose.model("student", StudentSchema);
+module.exports = { StudentSchemaValidate, StudentModel };
